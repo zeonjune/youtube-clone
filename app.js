3860d4d7bbb003451d3abc8d23101336d7587c6d
zeonjune <jihj0701@gmail.com>
2021-01-28 06:27:56 +0900

    video

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ import routes from './routes';
 
 app.set('view engine', 'pug');
 app.use(helmet());
+app.use(
+	helmet({
+		contentSecurityPolicy: false,
+	})
+);
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
---
+++ db.js
@@ -0,0 +1,67 @@
+export const videos = [
+	{
+		id: 324393,
+		title: 'Video awesome',
+		description: 'This is something I love',
+		views: 24,
+		videoFile:
+			'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+		creator: {
+			id: 121212,
+			name: 'Nicolas',
+			email: 'nico@las.com',
+		},
+	},
+	{
+		id: 3224393,
+		title: 'Video zzang',
+		description: 'This is something I love',
+		views: 24,
+		videoFile:
+			'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+		creator: {
+			id: 121212,
+			name: 'Nicolas',
+			email: 'nico@las.com',
+		},
+	},
+	{
+		id: 3243393,
+		title: 'Video haha',
+		description: 'This is something I love',
+		views: 24,
+		videoFile:
+			'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+		creator: {
+			id: 121212,
+			name: 'Nicolas',
+			email: 'nico@las.com',
+		},
+	},
+	{
+		id: 3243493,
+		title: 'Video wow',
+		description: 'This is something I love',
+		views: 24,
+		videoFile:
+			'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+		creator: {
+			id: 121212,
+			name: 'Nicolas',
+			email: 'nico@las.com',
+		},
+	},
+	{
+		id: 3245393,
+		title: 'Video good',
+		description: 'This is something I love',
+		views: 24,
+		videoFile:
+			'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+		creator: {
+			id: 121212,
+			name: 'Nicolas',
+			email: 'nico@las.com',
+		},
+	},
+];
